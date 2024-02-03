5dbbf55926810bb3c8f1e480a2fecd860cae99a8
Xavier Dunkley <xavierxv18@gmail.com>
2024-02-02 20:46:33 -0500

    created page for basket

--- backend/urls.py
+++ backend/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('basket/',include('basket.urls',namespace='basket')),
     path('',include('store.urls',namespace='store')),
 ]
 
